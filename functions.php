e9147117115d95b32c461577b2e6ddec2f3bfe14
Bruno Confortin <brunoconfortin@gmail.com>
2017-10-16 18:27:45 -0200

    Adicionado: meta infos e foto alternativa.

--- functions.php
+++ functions.php
@@ -83,11 +83,35 @@
 			'public' => true,
 			'hierarchical' => true
 		);
-
+		// register_taxonomy(nome, qual_custom_tipe, argumentos);
 		register_taxonomy('tipo_de_obra', 'obra', $args);
 	}
 
 	add_action( 'init' , 'register_taxonomy_tipos_de_obras' );
+
+	function register_taxonomy_localizacao() {
+		$singular = 'Estado e cidade';
+		$plural = 'Estados e cidades';
+
+		$labels = array(
+			'name' => $plural,
+			'singular_name' => $singular,
+			'view_item' => 'Ver ' . strtolower($singular),
+			'edit_item' => 'Editar ' . strtolower($singular),
+			'new_item' => 'Nova ' . strtolower($singular),
+			'add_new_item' => 'Adicionar nova ' . strtolower($singular)
+		);
+
+		$args = array(
+			'labels' => $labels,
+			'public' => true,
+			'hierarchical' => true
+		);
+		// register_taxonomy(nome, qual_custom_tipe, argumentos);
+		register_taxonomy('localizacao', 'obra', $args);
+	}
+
+	add_action( 'init' , 'register_taxonomy_localizacao' );
 
 	function is_selected_taxonomy($taxonomy, $search) {
 		if($taxonomy->slug === $search) {
@@ -95,122 +119,142 @@
 		}
 	}
 
-
-
-	/*
-	function adicionar_meta_info_imovel() {
+	function adicionar_meta_info_obra() {
 		add_meta_box(
-			'informacoes_imovel',
+			'informacoes_obra',
 			'Informações',
-			'informacoes_imovel_view',
-			'imovel',
+			'informacoes_obra_view',
+			'obra',
 			'normal',
 			'high'
 		);
 	}
 
-	add_action('add_meta_boxes', 'adicionar_meta_info_imovel');
+	add_action('add_meta_boxes', 'adicionar_meta_info_obra');
 
-	function informacoes_imovel_view( $post ) {
-		$imoveis_meta_data = get_post_meta( $post->ID ); ?>
+	function informacoes_obra_view( $post ) {
+		$obras_meta_data = get_post_meta( $post->ID ); ?>
 
 		<style>
-			.maluras-metabox {
-				display: flex;
-				justify-content: space-between;
+			.metabox-form {
+				margin-top: 15px;
 			}
 
-			.maluras-metabox-item {
-				flex-basis: 30%;
-
+			.metabox-form .form-group {
+				display: inline-block;
+				width: 49.7%;
+				margin-bottom: 15px;
 			}
 
-			.maluras-metabox-item label {
+			.metabox-form .form-group label {
+				display: block;
 				font-weight: 700;
+				margin-bottom: 5px;
+			}
+
+			.metabox-form .form-group input {
 				display: block;
-				margin: .5rem 0;
-
+				width: 100%;
 			}
-
-			.input-addon-wrapper {
-				height: 30px;
-				display: flex;
-				align-items: center;
-			}
-
-			.input-addon {
-				display: block;
-				border: 1px solid #CCC;
-				border-bottom-left-radius: 5px;
-				border-top-left-radius: 5px;
-				height: 100%;
-				width: 30px;
-				text-align: center;
-				line-height: 30px;
-				box-sizing: border-box;
-				background-color: #888;
-				color: #FFF;
-			}
-
-			.maluras-metabox-input {
-				height: 100%;
-				border: 1px solid #CCC;
-				border-left: none;
-				margin: 0;
-			}
-
 		</style>
-		<div class="maluras-metabox">
-			<div class="maluras-metabox-item">
-				<label for="maluras-preco-input">Preço:</label>
-				<div class="input-addon-wrapper">
-					<span class="input-addon">R$</span>
-					<input id="maluras-preco-input" class="maluras-metabox-input" type="text" name="preco_id"
-					value="<?= number_format($imoveis_meta_data['preco_id'][0], 2, ',', '.'); ?>">
-				</div>
-			</div>
 
-			<div class="maluras-metabox-item">
-				<label for="maluras-vagas-input">Vagas:</label>
-				<input id="maluras-vagas-input" class="maluras-metabox-input" type="number" name="vagas_id"
-				value="<?= $imoveis_meta_data['vagas_id'][0]; ?>">
+		<div class="metabox-form">
+			<div class="form-group">
+				<label for="obra_prazo">Prazo estipulado:</label>
+				<input type="text" name="obra_prazo" class="form-control" placeholder="" value="<?= $obras_meta_data['obra_prazo'][0]; ?>">
 			</div>
-
-			<div class="maluras-metabox-item">
-				<label for="maluras-banheiros-input">Banheiros:</label>
-				<input id="maluras-banheiros-input" class="maluras-metabox-input" type="number" name="banheiros_id"
-				value="<?= $imoveis_meta_data['banheiros_id'][0]; ?>">
+			<div class="form-group">
+				<label for="obra_conclusao">Tempo de conclusão:</label>
+				<input type="text" name="obra_conclusao" class="form-control" placeholder="" value="<?= $obras_meta_data['obra_conclusao'][0]; ?>">
 			</div>
-
-			<div class="maluras-metabox-item">
-				<label for="maluras-quartos-input">Quartos:</label>
-				<input id="maluras-quartos-input" class="maluras-metabox-input" type="number" name="quartos_id"
-				value="<?= $imoveis_meta_data['quartos_id'][0]; ?>">
+			<!-- <div class="form-group">
+				<label for="obra_img_desktop">Imagem 1920x450:</label>
+				<input type="file" name="obra_img_desktop" class="form-control" accept="image/*" value="<?= $obras_meta_data['obra_img_desktop'][0]; ?>">
 			</div>
-
+			<div class="form-group">
+				<label for="obra_img_mobile">Imagem 993x600:</label>
+				<input type="file" name="obra_img_mobile" class="form-control" accept="image/*" value="<?= $obras_meta_data['obra_img_mobile'][0]; ?>">
+			</div> -->
 		</div>
-	<?php
 
+	<?php }
+
+	function salvar_meta_info_obras( $post_id ) {
+		if( isset($_POST['obra_prazo']) ) {
+			update_post_meta( $post_id, 'obra_prazo', sanitize_text_field( $_POST['obra_prazo'] ) );
+		}
+		if( isset($_POST['obra_conclusao']) ) {
+			update_post_meta( $post_id, 'obra_conclusao', sanitize_text_field( $_POST['obra_conclusao'] ) );
+		}
+		// if( isset($_POST['obra_img_desktop']) ) {
+		// 	update_post_meta( $post_id, 'obra_img_desktop', $_POST['obra_img_desktop'] );
+		// }
+		// if( isset($_POST['obra_img_mobile']) ) {
+		// 	update_post_meta( $post_id, 'obra_img_mobile', $_POST['obra_img_mobile'] );
+		// }
 	}
 
-	function salvar_meta_info_imoveis( $post_id ) {
-		if( isset($_POST['preco_id']) ) {
-			update_post_meta( $post_id, 'preco_id', sanitize_text_field( $_POST['preco_id'] ) );
+	add_action('save_post', 'salvar_meta_info_obras');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	add_action( 'add_meta_boxes', 'listing_image_add_metabox' );
+	function listing_image_add_metabox () {
+		add_meta_box( 'listingimagediv', __( 'Imagem mobile (993x600)', 'text-domain' ), 'listing_image_metabox', 'obra', 'side', 'low');
+	}
+	function listing_image_metabox ( $post ) {
+		global $content_width, $_wp_additional_image_sizes;
+		$image_id = get_post_meta( $post->ID, '_listing_image_id', true );
+		$old_content_width = $content_width;
+		$content_width = 254;
+		if ( $image_id && get_post( $image_id ) ) {
+			if ( ! isset( $_wp_additional_image_sizes['post-thumbnail'] ) ) {
+				$thumbnail_html = wp_get_attachment_image( $image_id, array( $content_width, $content_width ) );
+			} else {
+				$thumbnail_html = wp_get_attachment_image( $image_id, 'post-thumbnail' );
+			}
+			if ( ! empty( $thumbnail_html ) ) {
+				$content = $thumbnail_html;
+				$content .= '<p class="hide-if-no-js"><a href="javascript:;" id="remove_listing_image_button" >' . esc_html__( 'Remover imagem', 'text-domain' ) . '</a></p>';
+				$content .= '<input type="hidden" id="upload_listing_image" name="_listing_cover_image" value="' . esc_attr( $image_id ) . '" />';
+			}
+			$content_width = $old_content_width;
+		} else {
+			$content = '<img src="" style="width:' . esc_attr( $content_width ) . 'px;height:auto;border:0;display:none;" />';
+			$content .= '<p class="hide-if-no-js"><a title="' . esc_attr__( 'Defina uma imagem', 'text-domain' ) . '" href="javascript:;" id="upload_listing_image_button" id="set-listing-image" data-uploader_title="' . esc_attr__( 'Defina uma imagem', 'text-domain' ) . '" data-uploader_button_text="' . esc_attr__( 'Defina uma imagem', 'text-domain' ) . '">' . esc_html__( 'Defina uma imagem', 'text-domain' ) . '</a></p>';
+			$content .= '<input type="hidden" id="upload_listing_image" name="_listing_cover_image" value="" />';
 		}
-		if( isset($_POST['vagas_id']) ) {
-			update_post_meta( $post_id, 'vagas_id', sanitize_text_field( $_POST['vagas_id'] ) );
-		}
-		if( isset($_POST['banheiros_id']) ) {
-			update_post_meta( $post_id, 'banheiros_id', sanitize_text_field( $_POST['banheiros_id'] ) );
-		}
-		if( isset($_POST['quartos_id']) ) {
-			update_post_meta( $post_id, 'quartos_id', sanitize_text_field( $_POST['quartos_id'] ) );
+		echo $content;
+	}
+	add_action( 'save_post', 'listing_image_save', 10, 1 );
+	function listing_image_save ( $post_id ) {
+		if( isset( $_POST['_listing_cover_image'] ) ) {
+			$image_id = (int) $_POST['_listing_cover_image'];
+			update_post_meta( $post_id, '_listing_image_id', $image_id );
 		}
 	}
 
-	add_action('save_post', 'salvar_meta_info_imoveis');
-	*/
+	function add_admin_scripts( $hook ) {
+		global $post;
+		wp_enqueue_script( 'myscript', get_stylesheet_directory_uri().'/js/scripts.js' );
+	}
+	add_action( 'admin_enqueue_scripts', 'add_admin_scripts', 10, 1 );
 
 
-	//flush_rewrite_rules();
+	function replace_featured_image_box() {
+	    remove_meta_box( 'postimagediv', 'obra', 'side' );
+	    add_meta_box('postimagediv', __('Imagem desktop (1920x450)'), 'post_thumbnail_meta_box', 'obra', 'side', 'low');
+	}
+	add_action('do_meta_boxes', 'replace_featured_image_box');
 ?>
--- single.php
+++ single.php
@@ -1,25 +1,45 @@
 <?php get_header(); ?>
 
+<?php
+if(have_posts()) {
+	while(have_posts()) {
+		the_post();
+		$obra_meta_data = get_post_meta( $post->ID );
+		//$obra_taxonomies = the_taxonomies( array( 'post' => $post->ID ) );
+		$avatar_id = $obra_meta_data['_listing_image_id'][0];
+		$metaImagem = wp_get_attachment_image( $avatar_id, 'full', false, array( 'class' => 'img-responsive hidden-md hidden-lg' ) );
+		$termLocalizacao = wp_get_post_terms($post->ID, 'localizacao');
+		$termLocalizacaoCidade = $termLocalizacao[0]->name;
+		$termLocalizacaoEstado = $termLocalizacao[1]->name;
+
+		$termTipoDeObra = wp_get_post_terms($post->ID, 'tipo_de_obra');
+		$termObra = $termTipoDeObra[0]->name; ?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12 padding-0">
+			<?php the_post_thumbnail('post-thumbnail', array('class' => 'img-responsive hidden-sm hidden-xs')); ?>
+			<?php echo $metaImagem; ?>
+		</div>
+	</div>
+</div>
 <div class="container-fluid">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 mtop-30">
-				<?php
-				if(have_posts()) {
-				    while(have_posts()) {
-				        the_post(); ?>
-						<article>
-							<section class="section-single-head">
-				    			<?php the_post_thumbnail('post-thumbnail', array('class' => 'img-responsive')); ?>
-							</section>
-							<section class="section-single-body padding-30 bg-fff">
-						        <h1 class="title"><?php the_title(); ?></h1>
-								<?php the_content(); ?>
-							</section>
-						</article>
-				    <?php }
-				}
-				?>
+				<article>
+					<section class="section-single-head">
+
+					</section>
+					<section class="section-single-body padding-30 bg-fff">
+				        <h1 class="title"><?php the_title(); ?></h1>
+						<p>Data de publicação: <?php the_date(); ?></p>
+						<?php the_content(); ?>
+						<p>Prazo da obra: <?php echo $obra_meta_data['obra_prazo'][0]; ?> dias</p>
+						<p>Obra concluída em:<?php echo $obra_meta_data['obra_conclusao'][0]; ?> dias</p>
+						<p>Cidade: <?php echo $termLocalizacaoCidade; ?></p>
+						<p>Estado: <?php echo $termLocalizacaoEstado; ?></p>
+					</section>
+				</article>
 			</div>
 			<div class="col-xs-12 col-sm-4 mtop-30">
 				<?php if ( is_active_sidebar( 'widgets_direita' ) ) : ?>
@@ -31,6 +51,6 @@
 		</div>
 	</div>
 </div>
-
+<?php } } ?>
 
 <?php get_footer(); ?>
